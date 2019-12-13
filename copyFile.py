b54ad9a7ca6de9748b01d938489a26d9dfcb3ff4
MattMark0 <57765461+mattmark0@users.noreply.github.com>
2019-12-13 09:55:21 -0600

    example - drag-drop a file into git-hub

---
+++ copyFile.py
@@ -0,0 +1,18 @@
+import shutil
+
+# copy file from here to there
+mySrc = "c:/temp/basic steps.txt.bak"
+myDst = "f:/tmp/basic steps.txt-4.bak"
+
+try:
+    shutil.copyfile(mySrc, myDst)
+except:
+    print(f'Error: copyfile failed')
+
+# copy directory from here to there
+mySrcDir = "c:/temp/tmp"
+myDstDir = "f:/tmp/tmp"
+try:
+    shutil.copytree(mySrcDir, myDstDir)
+except:
+    print('Error: copytree failed')
